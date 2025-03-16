679ea762a6b1acac11ddaa6cbd7dff75c2424292
S.Saha <soumabhasaha1509+github@gmail.com>
2025-03-16 22:55:56 +0530

    cascade delete done in product and images

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,3 +1,4 @@
+import { ProductImagesModel } from "./ProductImages.js";
 import { z } from "zod";
 import mongoose, { Schema, model } from "mongoose";
 export const Product = z.strictObject({
@@ -6,14 +7,42 @@ export const Product = z.strictObject({
     .min(4, 'name must have 4 or more chars')
     .max(40, 'name must be under 30 chars')
     .regex(/^[\x00-\x7F]+$/, 'invalid brand name'),
+  catagory: z
+    .enum([
+      'Electronics',
+      'Mens-Fashion',
+      'Womens-Fashion',
+      'Beauty & Personal Care',
+      'Home & Furniture',
+      'Grocery & Essentials',
+      'Toys & Baby Products',
+      'Books & Stationery',
+      'Pet Supplies'
+    ],
+      { required_error: "catagory must be provided!!!" }
+    )
+  ,
   description: z
     .string({ required_error: 'description is required' })
     .regex(/^[\x00-\x7F]+$/, 'invalid description'),
   details: z
-    .string({ required_error: 'password is required' })
-    .regex(/^[\x00-\x7F]+$/, 'invalid description'),
+    .string({ required_error: 'details is required' })
+    .regex(/^[\x00-\x7F]+$/, 'invalid details')
+    .refine((value) => {
+      /** 
+       * checking plain JSON [no nested is allowed!!!] 
+      */
+      try {
+        let object = JSON.parse(value);
+        for (const key in object) if (typeof object[key] === 'object') return false;
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }),
+  quantity: z.coerce.number({ required_error: 'quantity is required' }).int().positive().min(10),
   sellerId: z.instanceof(mongoose.Types.ObjectId),
-  price: z.coerce.number()
+  price: z.coerce.number().int().positive().min(50),
 });
 export type ProductType = z.infer<typeof Product>
 export const ProductSchema = new Schema<ProductType>({
@@ -33,6 +62,13 @@ export const ProductSchema = new Schema<ProductType>({
       message: (props: { value: string; }) => `${props.value} is not a valid description.`
     }
   },
+  catagory: {
+    type: String,
+    validate: {
+      validator: (v) => Product.pick({ catagory: true }).safeParse({ catagory: v }).success,
+      message: (prop: { value: string; }) => `${prop.value} is not a valid catagory`
+    }
+  },
   details: {
     type: String,
     required: [true, 'details is required'],
@@ -46,13 +82,30 @@ export const ProductSchema = new Schema<ProductType>({
     ref: "client_model",
     required: [true, "sellerId is required."]
   },
+  quantity: {
+    type: Number,
+    required: [true, 'quantity must be provided'],
+    validate: {
+      validator: (v) => Product.pick({ quantity: true }).safeParse({ quantity: v }).success,
+      message: (prop: { value: string; }) => `${prop.value} is not a valid quantity.`
+    }
+  },
   price: {
     type: Number,
-    required: true,
+    required: [true, "price can't be empty"],
     validate: {
       validator: value => Product.pick({ price: true }).safeParse({ price: value }).success,
       message: (props: { value: string; }) => `${props.value} is not a valid price.`
     }
   }
 }, { timestamps: true });
+
+ProductSchema.pre('deleteOne', { document: false, query: true }, async function (next) {
+  const product_id = this.getFilter()._id;
+  //console.log(product_id);
+  const result = await ProductImagesModel.deleteMany({ productId: product_id });
+  //console.log(result);
+  next();
+});
+
 export const ProductModel = model<ProductType>('product_model', ProductSchema); 
